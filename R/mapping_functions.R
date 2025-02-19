b97850c2fcfa7f9c206abab743197d09cd94ce41
Aaron Gullickson <aarongullickson@gmail.com>
2025-02-19 13:39:05 -0800

    Add first draft of write_planetary_data function

--- R/mapping_functions.R
+++ R/mapping_functions.R
@@ -5,7 +5,7 @@ planetary_system_events <- c("date", "nadirCharge", "zenithCharge")
 planet_events <- c("date", "population", "socioIndustrial", "hpg", "faction",
                    "hiringHall", "atmosphere", "pressure", "composition",
                    "water", "temperature", "lifeForm", "dayLength")
-planet_values <- c("sysPos", "name", "type", "orbitalDist", "pressure",
+planet_values <- c("name","sysPos", "type", "orbitalDist", "pressure",
                    "atmosphere", "composition", "gravity", "diameter",
                    "density", "dayLength", "yearLength", "temperature",
                    "water", "lifeForm", "smallMoons", "icon")
@@ -14,7 +14,8 @@ planet_values_sourceable <- c("spectralType", "primarySlot", "name", "type",
                               "gravity", "diameter", "density", "dayLength",
                               "yearLength", "temperature", "water", "lifeForm",
                               "population", "socioIndustrial", "hpg", "faction",
-                              "hiringHall", "capital", "smallMoons", "size")
+                              "hiringHall", "capital", "smallMoons", "size",
+                              "nadirCharge", "zenithCharge")
 
 read_planetary_data <- function(yaml_path) {
 
@@ -26,9 +27,11 @@ read_planetary_data <- function(yaml_path) {
   system_event_data <- NULL
   if(!is.null(raw_data$event)) {
     system_event_data <- purrr::map(raw_data$event, function(event) {
-      get_values(event, planetary_system_events)
+      get_values(event, planetary_system_events) |>
+        dplyr::mutate(date = lubridate::as_date(date))
     }) |>
-      dplyr::bind_rows()
+      dplyr::bind_rows() |>
+      dplyr::mutate(date = lubridate::as_date(date))
   }
 
   # get base planet data
@@ -74,6 +77,88 @@ read_planetary_data <- function(yaml_path) {
 
 }
 
+write_planetary_data <- function(planetary_system, path) {
+  # everything goes in lists of lists of lists
+
+  # start with overall system information
+  planet_list <- write_tibble_list(planetary_system$system,
+                                   planetary_system_values)
+
+  if(!is.null(planetary_system$system_events)) {
+    planet_list$event <- write_tibble_list(planetary_system$system_events,
+                                           planetary_system_events)
+  }
+
+  # get base planet information
+  planets <- write_tibble_list(planetary_system$planets, planet_values)
+
+  # now cycle through to add in landmasses, satellites, and events
+  for(i in 1:length(planets)) {
+    planet <- planets[[i]]
+
+    # add landmasses
+    landmass <- planetary_system$landmasses[[i]]
+    if(!is.null(landmass)) {
+      planet$landmass <- write_tibble_list(landmass, c("name", "capital"))
+    }
+
+    # add satellites
+    satellite <- planetary_system$satellites[[i]]
+    if(!is.null(satellite)) {
+      planet$satellite <- write_tibble_list(satellite, c("name", "size", "icon"))
+    }
+
+    # add planetary events
+    events <- planetary_system$planetary_events[[i]]
+    if(!is.null(events)) {
+      planet$event <- write_tibble_list(events, planet_events)
+    }
+
+    planets[[i]] <- planet
+  }
+
+  planet_list$planet <- planets
+
+  write_yaml(planet_list, file = path, indent.mapping.sequence = TRUE)
+}
+
+write_sourceable_value <- function(value, source) {
+  if(is.na(source)) {
+    return(value)
+  } else {
+    return(list(source = source, value = value))
+  }
+}
+
+write_tibble_list <- function(df, variables) {
+  df |>
+    pmap(function(...) {
+      row <- tibble(...)
+      data_list <- list()
+      for(variable in variables) {
+        if(variable %in% colnames(row) && !is.na(pull(row, variable))) {
+          if(variable %in% planet_values_sourceable) {
+            # we need to write a sourceable value
+            value <- pull(row, variable)
+            source <- pull(row, paste("source_", variable, sep = ""))
+            data_list[[variable]] <- write_sourceable_value(value, source)
+          } else {
+            # we can just write the value
+            # special hack for dates, which need to be converted back
+            # to characters
+            value <- pull(row, variable)
+            if(is.Date(value)) {
+              value <- as.character(value)
+            }
+            data_list[[variable]] <- value
+          }
+
+        }
+      }
+      return(data_list)
+    })
+}
+
 
 get_values <- function(data_source, value_names) {
   value_names |>
@@ -109,5 +194,4 @@ get_values <- function(data_source, value_names) {
     }) |>
     dplyr::bind_cols()
 }
-
 
